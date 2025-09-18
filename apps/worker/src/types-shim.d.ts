53390cffc1484840176c45b947cb3b67cb63843a
Diptanshu-215 <diptanshusaurav215@gmail.com>
2025-09-18 12:00:52 +0530

    fixes

---
+++ apps/worker/src/types-shim.d.ts
@@ -0,0 +1,2 @@
+declare module '@mail-ai/queue';
+declare module '@mail-ai/agents';
--- apps/worker/src/worker.ts
+++ apps/worker/src/worker.ts
@@ -1,23 +1,24 @@
-import { buildWorker, JobName } from '@mail-ai/queue';
+import { buildWorker } from '@mail-ai/queue';
+import { JobName, JobPayloadMap } from '../../../packages/queue/src/jobs';
 import { classifyEmail, generateDrafts, optimizeDraft, sendDraft, indexRAG } from '@mail-ai/agents';
 
-buildWorker(JobName.ClassifyMail, async job => {
+buildWorker(JobName.ClassifyMail, async (job: { data: JobPayloadMap[JobName.ClassifyMail] }) => {
   await classifyEmail(job.data.emailId);
 });
 
-buildWorker(JobName.GenerateDraft, async job => {
+buildWorker(JobName.GenerateDraft, async (job: { data: JobPayloadMap[JobName.GenerateDraft] }) => {
   await generateDrafts(job.data.emailId, job.data.tones);
 });
 
-buildWorker(JobName.OptimizeDraft, async job => {
+buildWorker(JobName.OptimizeDraft, async (job: { data: JobPayloadMap[JobName.OptimizeDraft] }) => {
   await optimizeDraft(job.data.draftId);
 });
 
-buildWorker(JobName.SendDraft, async job => {
+buildWorker(JobName.SendDraft, async (job: { data: JobPayloadMap[JobName.SendDraft] }) => {
   await sendDraft(job.data.draftId);
 });
 
-buildWorker(JobName.IndexRAG, async job => {
+buildWorker(JobName.IndexRAG, async (job: { data: JobPayloadMap[JobName.IndexRAG] }) => {
   await indexRAG(job.data.draftId);
 });
 
